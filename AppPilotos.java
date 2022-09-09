2278162b89677f505798a30dcd5adbc313e57554
barbaracarreiro <112444077+barbaracarreiro@users.noreply.github.com>
2022-09-09 19:36:56 -0300

    Initial commit

---
+++ AppPilotos.java
@@ -0,0 +1,89 @@
+package aplicativos;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AppPilotos {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        final int MAX_ELEMENTOS = 20;
+        int opcao, qtdCadastrados = 0;
+        Pessoa[] pilotos = new Pessoa[MAX_ELEMENTOS];
+        Scanner in = new Scanner(System.in);
+        int i = 0;
+
+        do {
+            System.out.println("\n****\nMENU\n****\n");
+            System.out.println("1 - Cadastrar piloto");
+            System.out.println("2 - Listar pilotos cadastrados");
+            System.out.println("3 - Localizar piloto pelo CPF");
+            System.out.println("4 - Aumentar espaço de armazenamento");
+            System.out.println("0 - Sair");
+            System.out.print("Opção: ");
+
+            opcao = in.nextInt();
+            in.nextLine(); // Tira o ENTER que ficou na entrada na instrução anterior
+
+            if (opcao == 1) {
+                // Se não tem mais espaço no vetor, caio fora
+                if (qtdCadastrados == MAX_ELEMENTOS) {
+                    System.out.println("\nNão há espaço para cadastrar novos pilotos.");
+                    voltarMenu(in);
+                    continue;
+                }
+
+                System.out.println("Informe o nome do piloto:");
+                String nome = in.nextLine();
+                
+                System.out.println("Informe o CPF do piloto:");
+                String CPF = in.nextLine();
+                
+                Pessoa pessoa = new Pessoa(nome, CPF);
+                pilotos[qtdCadastrados] = pessoa;
+                qtdCadastrados++;
+
+                System.out.println("\nPiloto cadastrado com sucesso.");
+                voltarMenu(in);
+            } else if (opcao == 2) {
+                
+                // Se não tem ninguém cadastrado no vetor, caio fora
+                if (qtdCadastrados == 0) {
+                    System.out.println("\nNão há pilotos cadastrados para exibir.");
+                    voltarMenu(in);
+                    continue;
+                }
+
+            
+               
+
+                // Exiba os pilotos aqui
+                
+
+                voltarMenu(in);
+            } else if (opcao == 3) {
+
+            } else if (opcao == 4) {
+                
+            }
+            else if (opcao != 0) {
+                System.out.println("\nOpção inválida!");
+            }
+        } while (opcao != 0);
+
+        System.out.println("Fim do programa!");
+
+        in.close();
+    }
+
+    private static void voltarMenu(Scanner in) throws InterruptedException, IOException {
+        System.out.println("\nPressione ENTER para voltar ao menu.");
+        in.nextLine();
+
+        // Limpa toda a tela, deixando novamente apenas o menu
+        if (System.getProperty("os.name").contains("Windows"))
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        else
+            System.out.print("\033[H\033[2J");
+        
+        System.out.flush();
+    }
+}
